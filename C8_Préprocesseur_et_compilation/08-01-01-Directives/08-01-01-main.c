f8a23239176f00a7df5c4f8f9f0025a4be4b2e1f
Emily <anny.baquerizolema@heig-vd.ch>
2024-05-22 00:41:57 +0200

    Chapitre 08 creation .c for futur exercice

---
+++ C8_Préprocesseur_et_compilation/08-01-01-Directives/08-01-01-main.c
@@ -0,0 +1,18 @@
+/**
+===============================================================================
+Exercice:               08-01-01
+Course:                 PRG2
+Year:                   2024
+Modification Author:    Emily Baquerizo
+Modification Date:      22.05.2024
+Version:                1.0
+===============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+   return EXIT_SUCCESS;
+}
