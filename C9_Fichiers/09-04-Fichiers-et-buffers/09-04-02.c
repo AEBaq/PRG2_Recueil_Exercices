622a6adb8b20d09c8aebcc7ed61264ced93f6496
Emily <anny.baquerizolema@heig-vd.ch>
2024-05-24 20:07:28 +0200

    TODO

--- C9_Fichiers/09-04-Fichiers-et-buffers/09-04-02.c
+++ C9_Fichiers/09-04-Fichiers-et-buffers/09-04-02.c
@@ -1,3 +1,18 @@
-//
-// Created by emily on 23.05.2024.
-//
+/**
+===============================================================================
+Exercice:               09-04-02 TODO
+Course:                 PRG2
+Year:                   2024
+Modification Author:    Emily Baquerizo
+Modification Date:      24.05.2024
+Version:                1.0
+===============================================================================
+*/
+
+#include <stdio.h> // printf()
+#include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILED
+
+int main(int argc, char *argv[]) {
+
+   return EXIT_SUCCESS;
+}
